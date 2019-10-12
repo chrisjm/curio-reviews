85ea5011c4155abc18a30cffef686a0e964215d5
Chris Mears <chris.mears@gmail.com>
2019-10-11 19:40:44 -0700

    Add update ability to Reviews table ⚙️

--- src/components/ReviewsTable.js
+++ src/components/ReviewsTable.js
@@ -1,12 +1,46 @@
 import React, { useState } from 'react';
 import { API, graphqlOperation } from 'aws-amplify';
-import { Pane, Table, Text, Button, majorScale, toaster } from 'evergreen-ui';
+import {
+  Pane,
+  Table,
+  Text,
+  TextInput,
+  Dialog,
+  SegmentedControl,
+  Textarea,
+  Button,
+  majorScale,
+  toaster,
+} from 'evergreen-ui';
+import DatePicker from 'react-date-picker';
+import moment from 'moment';
 import { listReviews } from '../graphql/queries';
+import { updateReview } from '../graphql/mutations';
 
 function ReviewsTable() {
-  const [result, setResult] = useState('No reviews loaded.');
-  const [loadReviewsButtonLabel, setloadReviewsButtonLabel] = useState('Load Reviews');
+  const [isUpdateDialogOpen, setIsUpdateDialogOpen] = useState(false);
   const [reviews, setReviews] = useState([]);
+  const [selectedReview, setSelectedReview] = useState({});
+
+  // Labels
+  const [loadReviewsButtonLabel, setloadReviewsButtonLabel] = useState('Load Reviews');
+  const [result, setResult] = useState('No reviews loaded.');
+
+  // Update Dialog
+  const [updatedUrl, setUpdatedUrl] = useState('');
+  const [updatedAuthor, setUpdatedAuthor] = useState('');
+  const [updatedReview, setUpdatedReview] = useState('');
+  const [updatedRating, setUpdatedRating] = useState(null);
+  const [updatedSource, setUpdatedSource] = useState('');
+  const [updatedDate, setUpdatedDate] = useState(null);
+
+  const ratingOptions = [
+    { label: '⭐', value: 1 },
+    { label: '⭐⭐', value: 2 },
+    { label: '⭐⭐⭐', value: 3 },
+    { label: '⭐⭐⭐⭐', value: 4 },
+    { label: '⭐⭐⭐⭐⭐', value: 5 },
+  ];
 
   async function loadReviews() {
     try {
@@ -20,6 +54,48 @@ function ReviewsTable() {
       console.error(error);
       toaster.danger(error.errors[0].message);
     }
+  }
+
+  async function updateSelectedReview() {
+    try {
+      // Required fields
+      let input = {
+        id: selectedReview.id,
+        url: updatedUrl,
+        author: updatedAuthor,
+        rating: updatedRating,
+      };
+
+      // Note: DynamoDB doesn't like empty string, so only add if necessary
+      if (updatedReview) input.description = updatedReview;
+      if (updatedSource) input.source = updatedSource;
+      if (updatedDate) input.date = moment(updatedDate).format('YYYY-MM-DD');
+
+      // Update review
+      const { data } = await API.graphql(graphqlOperation(updateReview, { input }));
+
+      // UI Feedback
+      toaster.success('Review updated successfully!');
+      closeUpdateDialog();
+    } catch (error) {
+      console.error(error);
+      toaster.danger(error.errors[0].message);
+    }
+  }
+
+  function showUpdateDialog(review) {
+    setSelectedReview(review);
+    setUpdatedUrl(review.url);
+    setUpdatedAuthor(review.author);
+    setUpdatedReview(review.description);
+    setUpdatedRating(review.rating);
+    setUpdatedSource(review.source);
+    setUpdatedDate(review.date);
+    setIsUpdateDialogOpen(true);
+  }
+
+  function closeUpdateDialog() {
+    setIsUpdateDialogOpen(false);
   }
 
   return (
@@ -36,7 +112,7 @@ function ReviewsTable() {
           </Table.Head>
           <Table.Body height={240}>
             {reviews.map(review => (
-              <Table.Row key={review.id} isSelectable onSelect={() => alert(review.description)}>
+              <Table.Row key={review.id} isSelectable onSelect={() => showUpdateDialog(review)}>
                 <Table.TextCell>{review.url}</Table.TextCell>
                 <Table.TextCell>{review.author}</Table.TextCell>
                 <Table.TextCell>{review.description}</Table.TextCell>
@@ -56,6 +132,62 @@ function ReviewsTable() {
           {loadReviewsButtonLabel}
         </Button>
       </Pane>
+      <Dialog
+        isShown={isUpdateDialogOpen}
+        title="Update Review"
+        onConfirm={updateSelectedReview}
+        onCloseComplete={closeUpdateDialog}
+        confirmLabel="Update Review"
+      >
+        <Pane>
+          <TextInput
+            name="url"
+            placeholder="https://curiomodern.com/products/..."
+            width="100%"
+            marginTop={majorScale(1)}
+            value={updatedUrl}
+            onChange={event => setUpdatedUrl(event.target.value)}
+          />
+          <TextInput
+            name="author"
+            placeholder="Customer Name"
+            width="100%"
+            marginTop={majorScale(1)}
+            value={updatedAuthor}
+            onChange={event => setUpdatedAuthor(event.target.value)}
+          />
+          <Textarea
+            name="description"
+            placeholder="Review"
+            width="100%"
+            marginTop={majorScale(1)}
+            value={updatedReview}
+            onChange={event => setUpdatedReview(event.target.value)}
+          />
+          <SegmentedControl
+            width="100%"
+            marginTop={majorScale(1)}
+            options={ratingOptions}
+            value={updatedRating}
+            onChange={value => setUpdatedRating(value)}
+          />
+          <TextInput
+            name="source"
+            placeholder="Source (ex. Etsy, Wayfair, Website)"
+            width="100%"
+            marginTop={majorScale(1)}
+            value={updatedSource}
+            onChange={event => setUpdatedSource(event.target.value)}
+          />
+          <Pane marginTop={majorScale(1)}>
+            <DatePicker
+              format="y-MM-dd"
+              onChange={date => setUpdatedDate(date)}
+              value={updatedDate}
+            />
+          </Pane>
+        </Pane>
+      </Dialog>
     </Pane>
   );
 }
