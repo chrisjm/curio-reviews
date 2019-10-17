818aeb48fb48c85e6f4e85c767a2e72c0fe2b4d0
Chris Mears <chris.mears@gmail.com>
2019-10-16 21:39:44 -0700

    Update config for Netlify 🚀

--- src/config.js
+++ src/config.js
@@ -1,26 +1,18 @@
-import awsmobile from './aws-exports';
-
 export default {
   awsconfig: {
-    aws_project_region: process.env.AWS_PROJECT_REGION || awsmobile.aws_project_region,
-    aws_cognito_identity_pool_id:
-      process.env.AWS_COGNITO_IDENTITY_POOL_ID || awsmobile.aws_cognito_identity_pool_id,
-    aws_cognito_region: process.env.AWS_COGNITO_REGION || awsmobile.aws_cognito_region,
-    aws_user_pools_id: process.env.AWS_USER_POOLS_ID || awsmobile.aws_user_pools_id,
-    aws_user_pools_web_client_id:
-      process.env.AWS_USER_POOLS_WEB_CLIENT_ID || awsmobile.aws_user_pools_web_client_id,
-    oauth: process.env.OAUTH || awsmobile.oauth,
-    aws_appsync_graphqlEndpoint:
-      process.env.AWS_APPSYNC_GRAPHQLENDPOINT || awsmobile.aws_appsync_graphqlEndpoint,
-    aws_appsync_region: process.env.AWS_APPSYNC_REGION || awsmobile.aws_appsync_region,
+    aws_project_region: process.env.AWS_PROJECT_REGION || 'us-west-2',
+    aws_cognito_identity_pool_id: process.env.AWS_COGNITO_IDENTITY_POOL_ID || null,
+    aws_cognito_region: process.env.AWS_COGNITO_REGION || 'us-west-2',
+    aws_user_pools_id: process.env.AWS_USER_POOLS_ID || null,
+    aws_user_pools_web_client_id: process.env.AWS_USER_POOLS_WEB_CLIENT_ID || null,
+    oauth: {},
+    aws_appsync_graphqlEndpoint: process.env.AWS_APPSYNC_GRAPHQLENDPOINT || null,
+    aws_appsync_region: process.env.AWS_APPSYNC_REGION || 'us-west-2',
     aws_appsync_authenticationType:
-      process.env.AWS_APPSYNC_AUTHENTICATIONTYPE || awsmobile.aws_appsync_authenticationType,
-    aws_content_delivery_bucket:
-      process.env.AWS_CONTENT_DELIVERY_BUCKET || awsmobile.aws_content_delivery_bucket,
+      process.env.AWS_APPSYNC_AUTHENTICATIONTYPE || 'AMAZON_COGNITO_USER_POOLS',
+    aws_content_delivery_bucket: process.env.AWS_CONTENT_DELIVERY_BUCKET || null,
     aws_content_delivery_bucket_region:
-      process.env.AWS_CONTENT_DELIVERY_BUCKET_REGION ||
-      awsmobile.aws_content_delivery_bucket_region,
-    aws_content_delivery_url:
-      process.env.AWS_CONTENT_DELIVERY_URL || awsmobile.aws_content_delivery_url,
+      process.env.AWS_CONTENT_DELIVERY_BUCKET_REGION || 'us-west-2',
+    aws_content_delivery_url: process.env.AWS_CONTENT_DELIVERY_URL || null,
   },
 };
