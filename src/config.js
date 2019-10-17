26e98b478a4f12982dfc3128e83da2fd6a0e4236
Chris Mears <chris.mears@gmail.com>
2019-10-16 22:09:16 -0700

    Utilize create-react-app's built-in .env handling 👊

--- src/config.js
+++ src/config.js
@@ -1,18 +1,16 @@
 export default {
   awsconfig: {
-    aws_project_region: process.env.AWS_PROJECT_REGION || 'us-west-2',
-    aws_cognito_identity_pool_id: process.env.AWS_COGNITO_IDENTITY_POOL_ID || null,
-    aws_cognito_region: process.env.AWS_COGNITO_REGION || 'us-west-2',
-    aws_user_pools_id: process.env.AWS_USER_POOLS_ID || null,
-    aws_user_pools_web_client_id: process.env.AWS_USER_POOLS_WEB_CLIENT_ID || null,
+    aws_project_region: process.env.REACT_APP_AWS_PROJECT_REGION,
+    aws_cognito_identity_pool_id: process.env.REACT_APP_AWS_COGNITO_IDENTITY_POOL_ID,
+    aws_cognito_region: process.env.REACT_APP_AWS_COGNITO_REGION,
+    aws_user_pools_id: process.env.REACT_APP_AWS_USER_POOLS_ID,
+    aws_user_pools_web_client_id: process.env.REACT_APP_AWS_USER_POOLS_WEB_CLIENT_ID,
     oauth: {},
-    aws_appsync_graphqlEndpoint: process.env.AWS_APPSYNC_GRAPHQLENDPOINT || null,
-    aws_appsync_region: process.env.AWS_APPSYNC_REGION || 'us-west-2',
-    aws_appsync_authenticationType:
-      process.env.AWS_APPSYNC_AUTHENTICATIONTYPE || 'AMAZON_COGNITO_USER_POOLS',
-    aws_content_delivery_bucket: process.env.AWS_CONTENT_DELIVERY_BUCKET || null,
-    aws_content_delivery_bucket_region:
-      process.env.AWS_CONTENT_DELIVERY_BUCKET_REGION || 'us-west-2',
-    aws_content_delivery_url: process.env.AWS_CONTENT_DELIVERY_URL || null,
+    aws_appsync_graphqlEndpoint: process.env.REACT_APP_AWS_APPSYNC_GRAPHQLENDPOINT,
+    aws_appsync_region: process.env.REACT_APP_AWS_APPSYNC_REGION,
+    aws_appsync_authenticationType: process.env.REACT_APP_AWS_APPSYNC_AUTHENTICATIONTYPE,
+    aws_content_delivery_bucket: process.env.REACT_APP_AWS_CONTENT_DELIVERY_BUCKET,
+    aws_content_delivery_bucket_region: process.env.REACT_APP_AWS_CONTENT_DELIVERY_BUCKET_REGION,
+    aws_content_delivery_url: process.env.REACT_APP_AWS_CONTENT_DELIVERY_URL,
   },
 };
